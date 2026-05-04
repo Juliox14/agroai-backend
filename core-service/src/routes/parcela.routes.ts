7146e4f70c56a9ffe09fe29e81507ebe97a4d440
Julian Castro <141683762+Juliox14@users.noreply.github.com>
2026-05-03 20:05:30 -0600

    add: controladores y otros cambios

--- core-service/src/routes/parcela.routes.ts
+++ core-service/src/routes/parcela.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
+import multer from "multer";
 import { obtenerParcelas, crearParcela, obtenerParcelaDetalle } from "../controllers/parcela.controller.js";
 import { verificarToken } from "../middlewares/auth.middleware.js";
 
 const router = Router();
+const upload = multer({ storage: multer.memoryStorage() });
 
 router.get('/', verificarToken, obtenerParcelas);
-router.post('/', verificarToken, crearParcela);
+router.post('/', verificarToken, upload.single('imagen'), crearParcela);
 router.get('/:id', verificarToken, obtenerParcelaDetalle);
 
 export default router;
